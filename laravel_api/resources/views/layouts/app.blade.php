305605007dcaf7949c5c25e4f15db662a23901a0
Ricardo Watanabe Bento <roicoroy@yahoo.com.br>
2024-01-04 17:30:22 +0000

    tasks

--- laravel_api/resources/views/layouts/app.blade.php
+++ laravel_api/resources/views/layouts/app.blade.php
@@ -1,45 +1,61 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+  <title>Laravel 10 Task List App</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script src="//unpkg.com/alpinejs" defer></script>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+  {{-- blade-formatter-disable --}}
+  <style type="text/tailwindcss">
+    .btn {
+      @apply rounded-md px-2 py-1 text-center font-medium text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50
+    }
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    .link {
+      @apply font-medium text-gray-700 underline decoration-pink-500
+    }
 
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body class="font-sans antialiased">
-        <x-banner />
+    label {
+      @apply block uppercase text-slate-700 mb-2
+    }
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+    input, 
+    textarea {
+      @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none
+    }
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+    .error {
+      @apply text-red-500 text-sm
+    }
+  </style>
+  {{-- blade-formatter-enable --}}
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+  @yield('styles')
+</head>
 
-        @stack('modals')
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
+  <h1 class="mb-4 text-2xl">@yield('title')</h1>
+  <div x-data="{ flash: true }">
+    @if (session()->has('success'))
+      <div x-show="flash"
+        class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700"
+        role="alert">
+        <strong class="font-bold">Success!</strong>
+        <div>{{ session('success') }}</div>
 
-        @livewireScripts
-    </body>
+        <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+            stroke-width="1.5" @click="flash = false"
+            stroke="currentColor" class="h-6 w-6 cursor-pointer">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </span>
+      </div>
+    @endif
+
+    @yield('content')
+  </div>
+</body>
+
 </html>

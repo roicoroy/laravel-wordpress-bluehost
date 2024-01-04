cec8c249e828cc46a6c916e76fc73a99fbb67b52
roicoroy <roicoroy@yahoo.com.br>
2024-01-04 13:36:23 -0700

    plugins deleted

--- wordpress/web/app/plugins/bluehost-wordpress-plugin/build/3.7.2/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-url'), 'version' => 'eb7b721753b366808a9b');
--- wordpress/web/app/plugins/woocommerce-payments/dist/woopay-express-button.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'e2392685e0465d74263e');
